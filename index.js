010f6c99d309447e8839479d09decc601dc6751c
Christophe <swolfschristophe@gmail.com>
2018-11-23 19:03:12 +0100

    updated package, - added express library - added canvas library

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
+// const { createCanvas, loadImage } = require('canvas');
+// const canvas = createCanvas(200, 200);
+// const ctx = canvas.getContext('2d');
+
+
 
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-//   res.send('Hello World!');
-    res.sendfile(__dirname + 'public/index.html');
+  // res.send('Hello World!');
+    res.sendfile(__dirname + '/public/index.html');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
